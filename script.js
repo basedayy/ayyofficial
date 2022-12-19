1d09397885db7fe58bf6275cda21729af5ef8aba
ayham999 <49962906+ayham999@users.noreply.github.com>
2022-12-20 00:57:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 document.getElementsByTagName("h1")[0].style.fontSize = "8vw";
+
+let titles = ['love', 'lain', '𝕃𝕒𝕚𝕟', 'Ｌａｉｎ'];
+    let currentTitle = 0;
+
+    function updateTitle() {
+      document.title = titles[currentTitle];
+      currentTitle = (currentTitle + 1) % titles.length;
+    }
+
+    setInterval(updateTitle, 1000);
